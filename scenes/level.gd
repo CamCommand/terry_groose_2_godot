36e7e6a487583169c520e70e0b96b84e7d4ff177
Cameron Drummond <48034712+CamCommand@users.noreply.github.com>
2026-02-07 21:01:51 -0500

    Add files via upload

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 
 @export var SpoonUpgradeCost: float = 100
@@ -8,13 +9,19 @@ extends Node2D
 @onready var Sand_Eatten_Label: Label = $Sand_Eatten_Label
 @onready var Sand_Dollar_Label: Label = $Sand_Dollar_Label
 
+#@onready var Spoon_Button: Button = $Spoon_Button
+
+@export var listItems: Array = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	Sand_Eatten_Label.text = "Sand Eatten: "
+	Sand_Dollar_Label.text = "Sand Dollars: "
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	if Player_Vars.s_label == "":
 		Sand_Eatten_Label.text = "Sand Eatten:"
 	else:
@@ -27,17 +34,38 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	if SpoonCheck == true && Player_Vars.Sand_Dollars >= SpoonUpgradeCost && SpoonCounter < 11:
-		Player_Vars.Sand_Dollars -= SpoonUpgradeCost
+	if SpoonCheck == true && Player_Vars.Sand_Total_Eatten >= SpoonUpgradeCost && SpoonCounter < 11:
+		Player_Vars.Sand_Total_Eatten -= SpoonUpgradeCost
+		
+		# sets the label text on button press
+		Sand_Eatten_Label.text = "Sand Eatten: " + str(Player_Vars.Sand_Total) + " .lbs"
+		Player_Vars.s_label = Sand_Eatten_Label.text
+		Sand_Dollar_Label.text = "Sand Dollars: $" + str(Player_Vars.Sand_Total_Eatten)
+		Player_Vars.s_label_d = Sand_Dollar_Label.text
+		
 	else:
 		$SpoonButton.text = "Buy Spoon:" + "\n"  + "100"
-		Player_Vars.Sand_Dollars -= SpoonUpgradeCost
+		Player_Vars.Sand_Total_Eatten -= SpoonUpgradeCost
 		SpoonCheck = true
 		Player_Vars.Sand = Player_Vars.Sand * 1.1
-
+		listItems.append("Spoon")
+		
+		# sets the label text on button press
+		Sand_Eatten_Label.text = "Sand Eatten: " + str(Player_Vars.Sand_Total) + " .lbs"
+		Player_Vars.s_label = Sand_Eatten_Label.text
+		Sand_Dollar_Label.text = "Sand Dollars: $" + str(Player_Vars.Sand_Total_Eatten)
+		Player_Vars.s_label_d = Sand_Dollar_Label.text
 
 func _on_spoon_timer_timeout() -> void:
-	if SpoonCheck == true:
+	# checking and setting Spoon Button conditions
+	if Player_Vars.Sand_Total >= 100 && !listItems.has("Spoon"):
+		$SpoonButton.disabled = false
+		$SpoonButton.modulate = Color(0.825, 0.741, 0.0, 1.0)
+	elif Player_Vars.Sand_Total < 100 && !listItems.has("Spoon"):
+		$SpoonButton.disabled = true
+		$SpoonButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
+		
+	if listItems.has("Spoon") && Player_Vars.Sand_Total >= 100:
 		$SpoonButton.text = "Spoon Upgrade:" + "\n"  + str(SpoonUpgradeCost)
 	else:
 		$SpoonButton.text = "Buy Spoon:" + "\n"  + "100"
---
+++ scenes/spoon_timer.gd
@@ -0,0 +1,18 @@
+@tool
+extends Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if Player_Vars.Sand_Total >= 100 && !Level_Vars.listItems.has("Spoon"):
+		#Level_Vars.Spoon_Button.disabled = true
+		#Level_Vars.Spoon_Button.modulate = Color(0.825, 0.741, 0.0, 1.0)
+	#elif Player_Vars.Sand_Total < 100 && !Level_Vars.listItems.has("Spoon"):
+		#Level_Vars.Spoon_Button.disabled = false
+		#Level_Vars.Spoon_Button.modulate = Color(0.824, 0.741, 0.0, 0.0)
+	pass
