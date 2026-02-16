6596c3b23b254981b831dd90bfecf0fe939bf328
Cameron Drummond <48034712+CamCommand@users.noreply.github.com>
2026-02-16 16:01:25 -0500

    Add files via upload

--- scenes/horse_coin.gd
+++ scenes/horse_coin.gd
@@ -8,29 +8,41 @@ var coin_rng := RandomNumberGenerator.new()
 var direction_x: float
 var speed
 
+@export var HorseCheck: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# setting the coin location at top of screen with random x valye 
 	coin_rng.randomize()
 	var width = get_viewport().get_visible_rect().size[0]
 	coin_pos_x = coin_rng.randi_range(300, width)
 	coin_pos_y = -120 #coin_rng.randi_range(-100, 0)
 	position = Vector2(coin_pos_x, coin_pos_y)
-	
+	# set coin speed and direction
 	speed = coin_rng.randi_range(350, 450)
 	direction_x = coin_rng.randf_range(-1, 1)
+	# check if horse has been bought already
+	#var horse = get_tree().get_first_node_in_group("horse_vars")
+	#if HorseCheck == false:
+		#horse.visible = false
+	#else:
+		#horse.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += Vector2(direction_x, 1.0) * speed * delta
 
 func _on_button_pressed() -> void:
-	Horse_Coin_Total += 1
-	$Button.disabled = true
-	$Button.visible = false
-	print(Horse_Coin_Total)
+		
+	var horse = get_tree().get_first_node_in_group("horse_vars")
+	if HorseCheck == false:
+		HorseCheck = true
+		horse.visible = true
+
+	Horse_Coin_Total = 1 # not adding properly, it's their own instance of the variable
+	# delete the coin
+	$".".queue_free()
 	
 
 func _on_body_entered(body: Node2D) -> void:
-	pass
-	#print("coin hit bottom")
-	#$Button.visible = true
+	$".".queue_free()
--- scenes/main.gd
+++ scenes/main.gd
@@ -25,29 +25,45 @@ var coin_scene: PackedScene = load("res://scenes/horse_coin.tscn")
 @export var PanCheck: bool
 @export var SuperPanCheck: bool
 
-#@onready var Spoon_Button Button = $Spoon_Button
+@export var ShovelUpgradeCost: float = 100
+@export var ShovelCounter: float
+@export var ShovelCheck: bool
+@export var SuperShovelCheck: bool
+
+@export var CLSUpgradeCost: float = 100
+@export var CLSCounter: float
+@export var CLSCheck: bool
+@export var FCLSCheck: bool
 
 @export var listItems: Array = []
+@export var Coin_Spawn_Time: float 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Sand_Ate.text = "Sand Ate: " + str(Sand_Total_Eaten) + " .lbs"
-	$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+	Sand_Total = 0
+	Sand_Total_Eaten = 0
+	# random coin drop time
+	var coin_drop:= RandomNumberGenerator.new()
+	randomize()
+	Coin_Spawn_Time = randf_range(100.05, 400.01)
+	$CoinTimer.wait_time = Coin_Spawn_Time
+	
+	$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01)) + ".lbs"
+	$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 	
 func _process(delta: float) -> void:
-
 	if $Sand_Ate.text == "":
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01)) + ".lbs"
 
 	if $Sand_Dollar.text == "":
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 
 	if Input.is_action_just_pressed("ui_left") && next_input == false:
 		Sand_Total += Sand
 		Sand_Total_Eaten += Sand
 		
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01)) + ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		
 		next_input = true
 	
@@ -55,8 +71,8 @@ func _process(delta: float) -> void:
 		Sand_Total += Sand
 		Sand_Total_Eaten += Sand
 		
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01)) + ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		
 		next_input = false
 
@@ -100,8 +116,8 @@ func _on_spoon_button_pressed() -> void:
 			SpoonUpgradeCost = SpoonUpgradeCost + (100 * SpoonCounter)
 			$SpoonButton.text = "Upgrade Spoon " + "\n" + str(SpoonUpgradeCost)
 
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.1
 		
 	elif SpoonCheck == true && Sand_Total >= SpoonUpgradeCost && SpoonCounter == 11:
@@ -112,8 +128,8 @@ func _on_spoon_button_pressed() -> void:
 		#add on screen text and or menu to display items here
 		
 		Sand_Total -= SpoonUpgradeCost
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.3
 		SpoonUpgradeCost = SpoonUpgradeCost + (200 * SpoonCounter)
 		SpoonCounter += 1
@@ -121,8 +137,8 @@ func _on_spoon_button_pressed() -> void:
 	
 	elif SuperSpoonCheck == true && Sand_Total >= SpoonUpgradeCost && SpoonCounter > 11:
 		Sand_Total -= SpoonUpgradeCost
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.35
 		SpoonUpgradeCost = SpoonUpgradeCost + (200 * SpoonCounter)
 		SpoonCounter += 1
@@ -130,8 +146,8 @@ func _on_spoon_button_pressed() -> void:
 
 	if Sand_Total >= 100 && SpoonCheck == false && SuperSpoonCheck == false:
 		Sand_Total -= 100
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		
 		listItems.append("Spoon")
 		SpoonCheck = true
@@ -142,8 +158,8 @@ func _on_spoon_button_pressed() -> void:
 		SpoonCounter += 1
 		$SpoonButton.text = "Upgrade Spoon " + "\n" + str(SpoonUpgradeCost)
 		
-	#SandModLabel.Text = "Sand Eatting Modifier: " & Math.Round(Sand, 2)
-
+	$Sand_Mult.text = "Consumption Rate: " + str(snapped(Sand, 0.01))
+	
 func _on_trowl_button_pressed() -> void:
 	#$TrowlButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
 	if TrowlCheck == true && Sand_Total >= TrowlUpgradeCost && TrowlCounter < 11:
@@ -158,8 +174,8 @@ func _on_trowl_button_pressed() -> void:
 			TrowlUpgradeCost = TrowlUpgradeCost + (150 * TrowlCounter)
 			$TrowlButton.text = "Upgrade Trowl " +"\n" + str(TrowlUpgradeCost)
 
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.2
 		
 	elif TrowlCheck == true && Sand_Total >= TrowlUpgradeCost && TrowlCounter == 11:
@@ -170,8 +186,8 @@ func _on_trowl_button_pressed() -> void:
 		#add on screen text and or menu to display items here
 		
 		Sand_Total -= TrowlUpgradeCost
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.4
 		TrowlUpgradeCost = TrowlUpgradeCost + (200 * TrowlCounter)
 		TrowlCounter += 1
@@ -179,8 +195,8 @@ func _on_trowl_button_pressed() -> void:
 	
 	elif SuperTrowlCheck == true && Sand_Total >= TrowlUpgradeCost && TrowlCounter > 11:
 		Sand_Total -= TrowlUpgradeCost
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.25
 		TrowlUpgradeCost = TrowlUpgradeCost + (200 * TrowlCounter)
 		TrowlCounter += 1
@@ -188,8 +204,8 @@ func _on_trowl_button_pressed() -> void:
 
 	if Sand_Total >= 1000 && TrowlCheck == false && SuperTrowlCheck == false:
 		Sand_Total -= 1000
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		
 		listItems.append("Trowl")
 		TrowlCheck = true
@@ -200,7 +216,7 @@ func _on_trowl_button_pressed() -> void:
 		TrowlCounter += 1
 		$TrowlButton.text = "Upgrade Trowl " +"\n" + str(TrowlUpgradeCost)
 		
-	#SandModLabel.Text = "Sand Eatting Modifier: " & Math.Round(Sand, 2)
+	$Sand_Mult.text = "Consumption Rate: " + str(snapped(Sand, 0.01))
 
 func _on_trowl_timer_timeout() -> void:
 # checking and setting Trowl Button conditions
@@ -242,8 +258,8 @@ func _on_pan_button_pressed() -> void:
 			PanUpgradeCost = PanUpgradeCost + (200 * PanCounter)
 			$PanButton.text = "Upgrade Pan " +"\n" + str(PanUpgradeCost)
 
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.2
 		
 	elif PanCheck == true && Sand_Total >= PanUpgradeCost && PanCounter == 11:
@@ -254,8 +270,8 @@ func _on_pan_button_pressed() -> void:
 		#add on screen text and or menu to display items here
 		
 		Sand_Total -= PanUpgradeCost
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.6
 		PanUpgradeCost = PanUpgradeCost + (200 * PanCounter)
 		PanCounter += 1
@@ -263,8 +279,8 @@ func _on_pan_button_pressed() -> void:
 	
 	elif SuperPanCheck == true && Sand_Total >= PanUpgradeCost && PanCounter > 11:
 		Sand_Total -= PanUpgradeCost
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		Sand = Sand * 1.35
 		PanUpgradeCost = PanUpgradeCost + (400 * PanCounter)
 		PanCounter += 1
@@ -272,8 +288,8 @@ func _on_pan_button_pressed() -> void:
 
 	if Sand_Total >= 3000 && PanCheck == false && SuperPanCheck == false:
 		Sand_Total -= 3000
-		$Sand_Ate.text = "Sand Ate: " + str(round(Sand_Total_Eaten))+ " .lbs"
-		$Sand_Dollar.text = "Sand Dollars: $" + str(round(Sand_Total))
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
 		
 		listItems.append("Pan")
 		PanCheck = true
@@ -283,6 +299,8 @@ func _on_pan_button_pressed() -> void:
 		PanUpgradeCost = 4000
 		PanCounter += 1
 		$PanButton.text = "Upgrade Pan " +"\n" + str(PanUpgradeCost)
+		
+	$Sand_Mult.text = "Consumption Rate: " + str(snapped(Sand, 0.01))
 
 func _on_pan_timer_timeout() -> void:
 # checking and setting Pan Button conditions
@@ -309,6 +327,174 @@ func _on_pan_timer_timeout() -> void:
 		$PanButton.text = "Buy " + "\n" + "Super Pan " + "\n" +  str(PanUpgradeCost)
 		$PanButton.disabled = true
 		$PanButton.modulate = Color(1.0, 1.0, 1.0, 1.0)	
+
+func _on_shovel_timer_timeout() -> void:
+# checking and setting Shovel Button conditions
+	if Sand_Total >= 10000 && !listItems.has("Shovel"):
+		$ShovelButton.text = "Buy Shovel " + "\n" + str(ShovelUpgradeCost)
+		$ShovelButton.disabled = false
+		$ShovelButton.modulate = Color(0.825, 0.741, 0.0, 1.0)
+	elif Sand_Total < 10000 && !listItems.has("Shovel"):
+		$ShovelButton.disabled = true
+		$ShovelButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
+		
+	if listItems.has("Shovel") && Sand_Total >= ShovelUpgradeCost:
+		$ShovelButton.disabled = false
+		$ShovelButton.modulate = Color(0.825, 0.741, 0.0, 1.0)
+	elif Sand_Total < ShovelUpgradeCost:
+		$ShovelButton.disabled = true
+		$ShovelButton.modulate = Color(1.0, 1.0, 1.0, 1.0)	
+		
+	if listItems.has("Shovel") && Sand_Total >= ShovelUpgradeCost && ShovelCounter == 11:
+		$ShovelButton.text = "Buy " + "\n" + "Super Shovel " + "\n" +  str(ShovelUpgradeCost)
+		$ShovelButton.disabled = false
+		$ShovelButton.modulate = Color(0.812, 0.145, 0.0, 1.0)
+	elif Sand_Total < ShovelUpgradeCost && ShovelCounter == 11:
+		$ShovelButton.text = "Buy " + "\n" + "Super Shovel " + "\n" +  str(ShovelUpgradeCost)
+		$ShovelButton.disabled = true
+		$ShovelButton.modulate = Color(1.0, 1.0, 1.0, 1.0)	
+	
+func _on_shovel_button_pressed() -> void:
+#$ShovelButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
+	if ShovelCheck == true && Sand_Total >= ShovelUpgradeCost && ShovelCounter < 11:
+		Sand_Total -= ShovelUpgradeCost
+		ShovelCounter += 1
+		
+	#multiplyer for bigger upgrades
+		if ShovelCounter == 11:
+			ShovelUpgradeCost = ShovelUpgradeCost * 2.5
+			$ShovelButton.text = "Buy " + "\n" + "Super Shovel " + "\n" + str(ShovelUpgradeCost)
+		else:
+			ShovelUpgradeCost = ShovelUpgradeCost + (600 * ShovelCounter)
+			$ShovelButton.text = "Upgrade Shovel " +"\n" + str(ShovelUpgradeCost)
+
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		Sand = Sand * 1.3
+		
+	elif ShovelCheck == true && Sand_Total >= ShovelUpgradeCost && ShovelCounter == 11:
+		listItems.append("Super Shovel")		
+		SuperShovelCheck = true
+		ShovelCheck = false	
+		
+		#add on screen text and or menu to display items here
+		
+		Sand_Total -= ShovelUpgradeCost
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		Sand = Sand * 1.8
+		ShovelUpgradeCost = ShovelUpgradeCost + (600 * ShovelCounter)
+		ShovelCounter += 1
+		$ShovelButton.text = "Upgrade " + "\n" + "Super Shovel " + "\n" + str(ShovelUpgradeCost)
+	
+	elif SuperShovelCheck == true && Sand_Total >= ShovelUpgradeCost && ShovelCounter > 11:
+		Sand_Total -= ShovelUpgradeCost
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		Sand = Sand * 1.55
+		ShovelUpgradeCost = ShovelUpgradeCost + (600 * ShovelCounter)
+		ShovelCounter += 1
+		$ShovelButton.text = "Upgrade " + "\n" + "Super Shovel " + "\n" + str(ShovelUpgradeCost)
+
+	if Sand_Total >= 10000 && ShovelCheck == false && SuperShovelCheck == false:
+		Sand_Total -= 10000
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		
+		listItems.append("Shovel")
+		ShovelCheck = true
+		
+		# update text with list of items here
+		Sand = Sand * 1.5
+		ShovelUpgradeCost = 20000
+		ShovelCounter += 1
+		$ShovelButton.text = "Upgrade Shovel " +"\n" + str(ShovelUpgradeCost)
+		
+	$Sand_Mult.text = "Consumption Rate: " + str(snapped(Sand, 0.01))
+
+func _on_cls_timer_timeout() -> void:
+# checking and setting Comically Large SPOON Button conditions
+	if Sand_Total >= 100000 && !listItems.has("Comically Large SPOON"):
+		$CLSButton.text = "Buy Comically " + "\n" + " Large SPOON " + "\n" + str(CLSUpgradeCost)
+		$CLSButton.disabled = false
+		$CLSButton.modulate = Color(0.825, 0.741, 0.0, 1.0)
+	elif Sand_Total < 10000 && !listItems.has("Comically Large SPOON"):
+		$CLSButton.disabled = true
+		$CLSButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
+		
+	if listItems.has("Comically Large SPOON") && Sand_Total >= CLSUpgradeCost:
+		$CLSButton.disabled = false
+		$CLSButton.modulate = Color(0.825, 0.741, 0.0, 1.0)
+	elif Sand_Total < CLSUpgradeCost:
+		$CLSButton.disabled = true
+		$CLSButton.modulate = Color(1.0, 1.0, 1.0, 1.0)	
+		
+	if listItems.has("Comically Large SPOON") && Sand_Total >= CLSUpgradeCost && CLSCounter == 11:
+		$CLSButton.text = "Buy Funnier " + "\n" + "Comically Large" + "\n" + " SPOON " + "\n" + str(CLSUpgradeCost)
+		$CLSButton.disabled = false
+		$CLSButton.modulate = Color(0.812, 0.145, 0.0, 1.0)
+	elif Sand_Total < CLSUpgradeCost && CLSCounter == 11:
+		$CLSButton.text = "Buy Funnier " + "\n" + "Comically Large" + "\n" + " SPOON " + "\n" + str(CLSUpgradeCost)
+		$CLSButton.disabled = true
+		$CLSButton.modulate = Color(1.0, 1.0, 1.0, 1.0)	
+
+func _on_cls_button_pressed() -> void:
+#$CLSButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
+	if CLSCheck == true && Sand_Total >= CLSUpgradeCost && CLSCounter < 11:
+		Sand_Total -= CLSUpgradeCost
+		CLSCounter += 1
+		
+	#multiplyer for bigger upgrades
+		if CLSCounter == 11:
+			CLSUpgradeCost = CLSUpgradeCost * 2.5
+			$CLSButton.text = "Buy Funnier " + "\n" + "Comically Large" + "\n" + " SPOON " + "\n" + str(CLSUpgradeCost)
+		else:
+			CLSUpgradeCost = CLSUpgradeCost + (550 * CLSCounter)
+			$CLSButton.text = "Upgrade Comically " + "\n" + " Large SPOON " + "\n" + str(CLSUpgradeCost)
+
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		Sand = Sand * 1.4
+		
+	elif CLSCheck == true && Sand_Total >= CLSUpgradeCost && CLSCounter == 11:
+		listItems.append("Funnier Comically Large SPOON")		
+		FCLSCheck = true
+		CLSCheck = false	
+		
+		#add on screen text and or menu to display items here
+		
+		Sand_Total -= CLSUpgradeCost
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		Sand = Sand * 1.8
+		CLSUpgradeCost = CLSUpgradeCost + (800 * CLSCounter)
+		CLSCounter += 1
+		$CLSButton.text = "Upgrade Funnier " + "\n" + "Comically Large" + "\n" + " SPOON " + "\n" + str(CLSUpgradeCost)
+	
+	elif FCLSCheck == true && Sand_Total >= CLSUpgradeCost && CLSCounter > 11:
+		Sand_Total -= CLSUpgradeCost
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		Sand = Sand * 1.75
+		CLSUpgradeCost = CLSUpgradeCost + (800 * CLSCounter)
+		CLSCounter += 1
+		$CLSButton.text = "Upgrade Funnier " + "\n" + "Comically Large" + "\n" + " SPOON " + "\n" + str(CLSUpgradeCost)
+
+	if Sand_Total >= 100000 && CLSCheck == false && FCLSCheck == false:
+		Sand_Total -= 100000
+		$Sand_Ate.text = "Sand Ate: " + str(snapped(Sand_Total_Eaten, 0.01))+ ".lbs"
+		$Sand_Dollar.text = "Sand Dollars: $" + str(snapped(Sand_Total, 0.01))
+		
+		listItems.append("Comically Large SPOON")
+		CLSCheck = true
+		
+		# update text with list of items here
+		Sand = Sand * 1.8
+		CLSUpgradeCost = 200000
+		CLSCounter += 1
+		$CLSButton.text = "Upgrade Comically " + "\n" + " Large SPOON " + "\n" + str(CLSUpgradeCost)
+		
+	$Sand_Mult.text = "Consumption Rate: " + str(snapped(Sand, 0.01))
 	
 func _on_coin_timer_timeout() -> void:
 	var coin = coin_scene.instantiate()
